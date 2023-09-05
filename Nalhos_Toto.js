bfc143b2d51f5f9498e03626d40975539a9c57aa
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-04 22:31:52 -0300

    Create Nalhos_Toto.js

---
+++ Nalhos_Toto.js
@@ -0,0 +1,5 @@
+import { ExponentialCost, FreeCost, LinearCost } from "./api/Costs";
+import { Localization } from "./api/Localization";
+import { BigNumber } from "./api/BigNumber";
+import { theory } from "./api/Theory";
+import { Utils } from "./api/Utils";
