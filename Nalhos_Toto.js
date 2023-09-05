24c92246a7648f1ba462244f1a634bd9c28e89e3
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-04 22:38:33 -0300

    Update Nalhos_Toto.js

--- Nalhos_Toto.js
+++ Nalhos_Toto.js
@@ -12,5 +12,19 @@ var version = "1.0.0";
 
 
 var currency;
-var j1, j2;
+var j1;
 
+var init = () => {
+    currency = theory.createCurrency();
+
+    ///////////////////
+    // Regular Upgrades
+
+    // j1
+    {
+        let getDesc = (level) => "j_1=" + getJ1(level).toString(0);
+        j1 = theory.createUpgrade(0, currency, new FirstFreeCost(new ExponentialCost(20, 1.2));
+        j1.getDescription = (_) => Utils.getMath(getDesc(k.level));
+        j1.getInfo = (amount) => Utils.getMathTo(getDesc(k.level), getDesc(k.level + 1));
+    }
+}
