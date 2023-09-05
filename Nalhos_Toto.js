115f12d839fbf8235b356321a3e985a6ed329fc0
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-04 22:33:15 -0300

    Update Nalhos_Toto.js

--- Nalhos_Toto.js
+++ Nalhos_Toto.js
@@ -3,3 +3,9 @@ import { Localization } from "./api/Localization";
 import { BigNumber } from "./api/BigNumber";
 import { theory } from "./api/Theory";
 import { Utils } from "./api/Utils";
+
+var id = "nalhos_toto";
+var name = "Nalhos Toto";
+var description = "Welcome to Nalhos Toto Theory!";
+var authors = "Skyhigh173";
+var version = "1.0.0";
