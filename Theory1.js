2e789a93d24cc12cd6b175fc4d3bd25f9137b95e
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-06 12:12:47 -0300

    Update Theory1.js

--- Theory1.js
+++ Theory1.js
@@ -4,11 +4,11 @@ import { parseBigNumber, BigNumber } from "../api/BigNumber";
 import { theory } from "../api/Theory";
 import { Utils } from "../api/Utils";
 
-var id = "classic_machine";
+var id = "ClassicMachine";
 var name = "Classic Machine Theory";
 var description = "The Classic Machine Theory";
 var authors = "JojoGames320";
-var version = 1;
+var version = "1.0.0";
 
 var currency, currency2;
 var s1;
