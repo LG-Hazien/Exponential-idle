ee0f57ce5acafa59c33fdfc6f37fd1b8cf06d996
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-06 17:09:02 -0300

    Update Castle_deck_simulator.js

--- Castle_deck_simulator.js
+++ Castle_deck_simulator.js
@@ -6,5 +6,29 @@ var description = "No theory description :("
 var version = "v1.0.0";
 
 const PLAYS = BigNumber.ZERO;
+const DECKS = BigNumber.ZERO;
 
-code
+fuction fuct(value){
+  if (value == 1) {
+    DECKS += BigNumber.ONE
+  }
+}
+
+var getMainPanel = () => {
+    if (Antimatter >= bf('1.79e308')) {
+        return ui.createButton({text:"Big Crunch", fontSize: 30, onClicked: BigCrunch})
+    }
+    let btns = (name, row, onc) => {
+        return ui.createButton({text:name,row:row,fontSize:20,onClicked:()=>{fuct(onc); reloadUI(); Sound.playClick(); }});
+    }
+    return ui.createStackLayout({children:[ui.createScrollView({
+        children: [
+            ui.createGrid({
+                rowDefinitions: ["1*","1*","1*","4","1*","4","1*"],
+                children: [
+                    btns("New Deck",0,1),
+                ]
+            })
+        ]
+    })]})
+}
