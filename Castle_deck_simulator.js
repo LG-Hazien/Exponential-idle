6461ca30a97ff7ea7d154b5369c25d636623c2c5
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-06 17:21:27 -0300

    Update Castle_deck_simulator.js

--- Castle_deck_simulator.js
+++ Castle_deck_simulator.js
@@ -3,6 +3,7 @@ import { ui } from ".../api/ui/UI";
 var id = "OuO";
 var name = "Castle deck simulator";
 var description = "No theory description :("
+var authors = "JojoGames";
 var version = "v1.0.0";
 
 const PLAYS = BigNumber.ZERO;
