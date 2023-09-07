d4bbbbcbfb3c81e92bffaf69b9762dffc746e147
JojoGamesLol <143434175+JojoGamesLol@users.noreply.github.com>
2023-09-07 12:58:58 -0300

    Update IdleClicker.js

--- IdleClicker.js
+++ IdleClicker.js
@@ -1,2 +1,8 @@
+import { ExponentialCost, FirstFreeCost, LinearCost } from "../api/Costs";
+import { Localization } from "../api/Localization";
+import { parseBigNumber, BigNumber } from "../api/BigNumber";
+import { theory } from "../api/Theory";
+import { Utils } from "../api/Utils";
+
 var id = "idleclicker"
 var name = "Idle Clicker"
